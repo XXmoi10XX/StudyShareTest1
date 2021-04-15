fd41abe94827e5faece9b55f63ed81c13f79f2a2
masann <masaaki70@icloud.com>
2021-04-15 10:27:57 +0900

    ゲストログイン追加

---
+++ .history/app/resources/views/auth/login.blade_20210415095032.php
@@ -0,0 +1,72 @@
+@extends('app')
+
+@section('title', 'ログイン')
+
+@section('content')
+
+<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.11/css/mdb.min.css" rel="stylesheet">
+<div class="container">
+    <div class="row">
+        <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
+            <h1 class="text-center"><a class="text-dark" href="/">Share Study</a></h1>
+            <div class="card mt-3">
+                <div class="card-body text-center">
+                    <h2 class="h3 card-title text-center mt-2">ログイン</h2>
+
+                    @include('error_card_list')
+
+                    <div class="card-text">
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+
+                            <div class="md-form">
+                                <label for="email">メールアドレス</label>
+                                <input class="form-control" type="text" id="email" name="email" required value="{{ old('email') }}">
+                            </div>
+
+                            <div class="md-form">
+                                <label for="password">パスワード</label>
+                                <input class="form-control" type="password" id="password" name="password" required>
+                            </div>
+
+
+                            <input type="hidden" name="remember" id="remember" value="on">
+
+
+                            <button type="submit" class="btn btn-primary btn-block mb-4">ログイン</button>
+
+
+                        </form>
+
+
+
+
+                        <input class="form-control" type="hidden" id="email" name="email" required value="test@example.com">
+
+
+
+                        <input class="form-control" type="hidden" id="password" name="password" required value="test@example.com">
+
+
+
+                        <input type="hidden" name="remember" id="remember" value="on">
+
+
+                        <button form="login-button" type="submit" class="btn btn-primary btn-block mb-4">ゲストログイン</button>
+
+                        <form id="login-button" method="POST" action="{{ route('login') }}">
+                            @csrf
+
+                        </form>
+
+                        <div class="mt-0">
+                            <a href="{{ route('register') }}" class="card-text">ユーザー登録はこちら</a>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ vendor/composer/installed.php
@@ -0,0 +1,87 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '486123c19ddf308a446e3d29127280dbd6470ba5',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '486123c19ddf308a446e3d29127280dbd6470ba5',
+    ),
+    'beyondcode/laravel-er-diagram-generator' => 
+    array (
+      'pretty_version' => '1.5.0',
+      'version' => '1.5.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2578ea947f1b9030221a30803385e4b4758ca323',
+    ),
+    'doctrine/cache' => 
+    array (
+      'pretty_version' => '1.10.2',
+      'version' => '1.10.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '13e3381b25847283a91948d04640543941309727',
+    ),
+    'doctrine/dbal' => 
+    array (
+      'pretty_version' => '2.13.0',
+      'version' => '2.13.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '67d56d3203b33db29834e6b2fcdbfdc50535d796',
+    ),
+    'doctrine/deprecations' => 
+    array (
+      'pretty_version' => 'v0.5.3',
+      'version' => '0.5.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9504165960a1f83cc1480e2be1dd0a0478561314',
+    ),
+    'doctrine/event-manager' => 
+    array (
+      'pretty_version' => '1.1.1',
+      'version' => '1.1.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '41370af6a30faa9dc0368c4a6814d596e81aba7f',
+    ),
+    'nikic/php-parser' => 
+    array (
+      'pretty_version' => 'v4.10.4',
+      'version' => '4.10.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c6d052fc58cb876152f89f532b95a8d7907e7f0e',
+    ),
+    'phpdocumentor/graphviz' => 
+    array (
+      'pretty_version' => '1.0.4',
+      'version' => '1.0.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a906a90a9f230535f25ea31caf81b2323956283f',
+    ),
+  ),
+);
